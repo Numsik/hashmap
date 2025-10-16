8543ddc3c5726d9ee2eda5a233d2ab9e778231e1
Numsik <honzik.klusek.myto@gmail.com>
2025-10-16 12:19:18 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+function stringToNumber(string) {
+  let hashCode = 0;
+
+  const primeNumber = 31;
+  for (let i = 0; i < string.length; i++) {
+    hashCode = primeNumber * hashCode + string.charCodeAt(i);
+  }
+
+  return hashCode;
+}
+console.log(stringToNumber('Honza'))
