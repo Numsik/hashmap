827b0bc1b1d497e3f0555b2509ff7b38d186d124
Numsik <honzik.klusek.myto@gmail.com>
2025-10-16 13:23:38 +0200

    add set

--- index.js
+++ index.js
@@ -6,5 +6,33 @@ class Hashmap{
         this.buckets = new Array(this.capacity).fill(null).map(() => []);
 
     }
+    hash(key){
+        let hashCode = 0;
+        for (let i = 0; i < key.length; i++){
+            const char = key.charCodeaT(i)
+            hashCode = hashCode * 31 + char
+        }
+        return hashCode % this.capacity;
+
+    }
+    set(key, value){
+
+        const index = this.hash(key);
+        const bucket = this.buckets[index];
+
+        for (let item of bucket){
+            if(item.key === key){
+                item.value = value;
+                return;
+            }
+        }
+        bucket.push({key, value})
+        this.size++;
+
+        if (this.size / this.capacity > this.loadfactor){
+            this.resize(this.capacity * 2)
+        }
+
+    }
         
 }
